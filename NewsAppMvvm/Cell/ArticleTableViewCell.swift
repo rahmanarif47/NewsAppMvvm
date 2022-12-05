cb3963808167cdf4ad6abd44fa72c02054e6e304
Arif Rahman Sidik <rahmanarif.9764@gmail.com>
2022-12-05 13:50:23 +0700

    fetch and populate news api

---
+++ NewsAppMvvm/Cell/ArticleTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  ArticleTableViewCell.swift
+//  NewsAppMvvm
+//
+//  Created by Arif Rahman Sidik on 05/12/22.
+//
+
+import Foundation
+import UIKit
+
+class ArticleTableViewCell : UITableViewCell {
+    @IBOutlet var titleLabel : UILabel!
+    @IBOutlet var descriptionLabel : UILabel!
+}
--- NewsAppMvvm/Controllers/NewsTableViewController.swift
+++ NewsAppMvvm/Controllers/NewsTableViewController.swift
@@ -12,6 +12,9 @@ import RxCocoa
 
 class NewsTableViewController : UITableViewController {
     
+    let disposedBag = DisposeBag()
+    private var articleListVM : ArticleListViewModel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -20,9 +23,43 @@ class NewsTableViewController : UITableViewController {
         populateNews()
     }
     
-    private func populateNews(){
-        let resource = Resource<ArticleResponse>(url: URL(string: "") ?? "")
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
     
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.articleListVM == nil ? 0: self.articleListVM.articlesVM.count
+    }
     
+    private func populateNews(){
+        let resource = Resource<ArticleResponse>(url: URL(string: "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=1d61b36a93c04c3c81039cb705762ff6")!)
+        
+        URLRequest.load(resource: resource)
+            .subscribe(onNext: { articleResponse in
+                let articles = articleResponse.articles
+                self.articleListVM = ArticleListViewModel(articles)
+                
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+            }).disposed(by: disposedBag)
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "ArticleTableViewCell", for: indexPath) as? ArticleTableViewCell else {
+            fatalError("ArticleTableViewCell is not found")
+        }
+        
+        let articleVM = self.articleListVM.articleAt(indexPath.row)
+        
+        articleVM.title.asDriver(onErrorJustReturn: "")
+            .drive(cell.titleLabel.rx.text)
+            .disposed(by: self.disposedBag)
+        
+        articleVM.description.asDriver(onErrorJustReturn: "")
+            .drive(cell.descriptionLabel.rx.text)
+            .disposed(by: self.disposedBag)
+        
+        return cell
+    }
 }
